acc3e9546423be3c5ae6840a57540ad9a6542914
Leonardo Mörlein <git@irrelefant.net>
2021-03-27 22:10:49 +0100

    add output raw-jsonl
    This output takes the respondd response as sent by the node and includes
    it in a Line-Delimited JSON (JSONL) document. In this format each line
    can be interpreted as separate JSON element, which is useful for json
    streaming. The first line is json object holding the timestamp and
    version of the file. Then there follows one line for each node, each
    containing a json object.
    
    An example output looks like this:
    
        {"version":1,"updated_at":"2021-03-27T21:58:48+0100","format":"raw-nodes-jsonl"}
        {"firstseen": ..., "lastseen": ..., "online":true, "statistics": {...}, "nodeinfo": {...}, "neighbours":null, "custom_fields":null}
        {"firstseen": ..., "lastseen": ..., "online":true, "statistics": {...}, "nodeinfo": {...}, "neighbours":null, "custom_fields":null}
        {"firstseen": ..., "lastseen": ..., "online":true, "statistics": {...}, "nodeinfo": {...}, "neighbours":null, "custom_fields":null}
        {"firstseen": ..., "lastseen": ..., "online":true, "statistics": {...}, "nodeinfo": {...}, "neighbours":null, "custom_fields":null}
        ...
    
    Signed-off-by: Leonardo Mörlein <git@irrelefant.net>

--- output/all/main.go
+++ output/all/main.go
@@ -2,6 +2,7 @@ package all
 
 import (
 	_ "github.com/FreifunkBremen/yanic/output/geojson"
+	_ "github.com/FreifunkBremen/yanic/output/raw-jsonl"
 	_ "github.com/FreifunkBremen/yanic/output/meshviewer"
 	_ "github.com/FreifunkBremen/yanic/output/meshviewer-ffrgb"
 	_ "github.com/FreifunkBremen/yanic/output/nodelist"
